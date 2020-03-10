ba4530ff5e4ef4ef76db7da0f435277cdbbc6134
SALVADOR GARCÍA G <61257815+GARS23-s@users.noreply.github.com>
2020-03-09 18:09:09 -0600

    Add files via upload

---
+++ CALCULADORA P2.py
@@ -0,0 +1,8 @@
+from tkinter import*
+ventana=Tk()
+ventana.title("CALCULADORA")
+ventana.configure(background="gray20")
+ventana.geometry("392x488")
+color boton=("gray50")
+cn=("white")
+actb="LightCyan3"
